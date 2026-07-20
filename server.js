c994c78ddf2fa3e32f10f9dffebb8f2c04921f0e
ddegraaf-max <d.degraaf@creditline.nl>
2026-07-20 20:38:30 +0200

    ertre

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const SITE_VERSION = '1.4.0'; // cache-busting ?v=
+const SITE_VERSION = '1.4.1'; // cache-busting ?v=
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
